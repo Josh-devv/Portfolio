7053235947f9d1966aa5e32e86a821e70dbec560
josh sofela <joshuasofela@gmail.com>
2024-02-04 23:27:47 +0100

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/index.css';
+import { ProjectProvider } from './Components/Context/ProjectContext.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -8,7 +9,9 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <App />
-    <ReactQueryDevtools />
-  </QueryClientProvider>
+      <ProjectProvider>
+        <App />
+        <ReactQueryDevtools />
+      </ProjectProvider>
+    </QueryClientProvider>
 );
