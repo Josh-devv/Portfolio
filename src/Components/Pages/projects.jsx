1b015edf30fb303bc1253373d44dc8eba51d731d
josh sofela <joshuasofela@gmail.com>
2024-02-05 13:24:54 +0100

    mew commit

---
+++ src/Components/Pages/projects.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import useFadeUpEffect from '../Hooks/fadeUp';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import useFetch from '../Hooks/useFetch';
+
+
+const Projects = () => {
+
+
+  return (
+<>
+</>
+  );
+};
+
+export default Projects;
