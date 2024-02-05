395375b4c8eb160c2d105ba5d68dfee66343f9ab
josh sofela <joshuasofela@gmail.com>
2024-02-05 12:38:54 +0100

    new commit

--- src/Components/Pages/projects.jsx
+++ src/Components/Pages/projects.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+const { VITE_API_SERVER } = import.meta.env;
 import useFadeUpEffect from '../Hooks/fadeUp';
 import Skeleton from 'react-loading-skeleton';
-import { useProjectContext } from '../Context/ProjectContext';
 import 'react-loading-skeleton/dist/skeleton.css';
 import useFetch from '../Hooks/useFetch';
 
@@ -12,11 +11,9 @@ export default function Projects() {
 
   //const { data } = useFetch(`${VITE_API_SERVER}/get_projects`, 'getProjects');
   const [backend, setBackend] = useState([{}]);
- // const [projects, setProjects] = useState([]);
-  //const [loading, setLoading] = useState(true);
-  const { state, dispatch } = useProjectContext();
-  const { projects, loading, error } = state;
-  //const { VITE_API_SERVER } = import.meta.env;
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   //const { projects } = data || {};
   const element = useRef(null);
   useFadeUpEffect(element);
@@ -33,64 +30,56 @@ export default function Projects() {
 
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/api');
-        const data = await response.json();
-        dispatch({ type: 'FETCH_PROJECTS_SUCCESS', payload: data.projects });
-      } catch (error) {
-        dispatch({ type: 'FETCH_PROJECTS_FAILURE', payload: error.message });
-      }
-    };
+    fetch("http://localhost:3000/api")
+      .then(response => response.json())
+      .then(data => {
+        console.log('API Data:', data);
+        setProjects(data?.projects || []);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
+  }, []);
 
-    fetchData();
-  }, [dispatch]);
 
   return (
-    <section ref={element} className="fade translate-y-[100px] pt-5 max-md:pb-0 pb-24 opacity-5">
-      <div className="max-w-7xl mx-auto px-8 xl:px-0">
-        <h2 className="text-3xl font-bold dark:text-white">Projects</h2>
-        <p className="my-6">
-          Among countless projects, these are my proudest achievements —{' '}
-          <span className="italic">a testament to my dedication and creativity.</span>
-        </p>
+   
 
-        {loading ? (
-          <Skeleton containerClassName="flex-1" count={5} height={60} borderRadius={'10px'} baseColor="gray" />
-        ) : (
-          <section className={`projects ${projects.length <= 0 ? 'flex' : 'grid'} items-center justify-center`}>
-            {projects.length > 0 ? (
-              projects.map((data, i) => (
-                <Link
-                  key={i}
-                  to={`/projects/${data.id}`}
-                  className="border border-transparent bg-zinc-50 flex items-center gap-3 hover:border-gray-200 cursor-pointer p-4 rounded-lg dark:bg-zinc-800 dark:hover:border-zinc-600"
-                >
-                  <div className="bg-gray-100 p-2 rounded-md min-w-[60px] min-h-[60px] flex items-center justify-center">
-                    {data.images.map((image, index) => (
-                      <img
-                        key={index}
-                        src={`http://localhost:3000${image}`}
-                        alt={`Image ${index}`}
-                        className="w-[60px]"
-                      />
-                    ))}
-                  </div>
-                  <div>
-                    <h4 className="font-medium mb-2 dark:text-white">{data.name}</h4>
-                    <p className="text-sm dark:text-white">{data.tagline}</p>
-                  </div>
-                </Link>
-              ))
-            ) : (
-              <p>No projects available</p>
-            )}
-          </section>
-        )}
-      </div>
+    <section ref={element} className="fade translate-y-[100px] pt-5 max-md:pb-0 pb-24 opacity-5">
+    <div className="max-w-7xl mx-auto px-8 xl:px-0">
+      <h2 className="text-3xl font-bold dark:text-white">Projects</h2>
+      <p className="my-6">
+        Among countless projects, these are my proudest achievements — <span className="italic">a testament to my dedication and creativity.</span>
+      </p>
+
+      <section className={`projects ${projects.length <= 0 ? 'flex' : 'grid'} items-center justify-center`}>
+      {loading ? (
+        <Skeleton containerClassName="flex-1" count={5} height={60} borderRadius={'10px'} baseColor="gray" />
+      ) : (
+        projects.map((data, i) => (
+          <Link key={i} to={`/projects/${data.id}`} className="border border-transparent bg-zinc-50 flex items-center gap-3 hover:border-gray-200 cursor-pointer p-4 rounded-lg dark:bg-zinc-800 dark:hover:border-zinc-600">
+            <div className="bg-gray-100 p-2 rounded-md min-w-[60px] min-h-[60px] flex items-center justify-center">
+              {data.images.map((image, index) => (
+                <img key={index} src={`http://localhost:3000${image}`} alt={`Image ${index}`} className="w-[60px]" />
+              ))}
+            </div>
+            <div>
+              <h4 className="font-medium mb-2 dark:text-white">{data.name}</h4>
+              <p className="text-sm dark:text-white">{data.tagline}</p>
+            </div>
+          </Link>
+        ))
+      )}
     </section>
+
+    </div>
+  </section>
+  
   );
-}
+};
+
 
 /*
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/index.css';
-import { ProjectProvider } from './Components/Context/ProjectContext.jsx';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-      <ProjectProvider>
+ 
+      
         <App />
-        <ReactQueryDevtools />
-      </ProjectProvider>
-    </QueryClientProvider>
+       
 );
