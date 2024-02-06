10fed19757a5809e2f2c922672d6ef68134f8fd9
josh sofela <joshuasofela@gmail.com>
2024-02-06 18:25:31 +0100

    new comit

--- src/Components/Pages/projects.jsx
+++ src/Components/Pages/projects.jsx
@@ -7,10 +7,12 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import useFetch from '../Hooks/useFetch';
 import { useQuery } from '@tanstack/react-query';
 
+
 export default function Projects() {
 
   const { data } = useFetch(`${VITE_API_SERVER}/project`, 'getProjects');
-  const [projects, setProjects] = useState([]);
+  //const { projects } = data || {};
+  const [projects, setProjects] = useState([{}])
   const [loading, setLoading] = useState(true);
   const element = useRef(null);
   useFadeUpEffect(element);
@@ -38,8 +40,11 @@ export default function Projects() {
     };
 
     fetchData();
-  }, []);
+}, []);
 
+
+
+console.log(data);
   useEffect(() => {
     document.title = 'Projects | Sofela Joshua';
 
@@ -48,34 +53,42 @@ export default function Projects() {
     };
   }, []);
 
+
+
+
   return (
     <section ref={element} className="fade translate-y-[100px] opacity-5">
+     {loading ? (
+    <Skeleton containerClassName="flex-1" count={5} height={60} borderRadius={'10px'} baseColor="gray" />
+) : (
+    projects.map((project, i) => (
+        <Link key={i} to={`/projects/${project.id}`} className="border border-transparent bg-zinc-50 flex items-center gap-3 hover:border-gray-200 cursor-pointer p-4 rounded-lg dark:bg-zinc-800 dark:hover:border-zinc-600">
+            <div className="bg-gray-100 p-2 rounded-md min-w-[60px] min-h-[60px] flex items-center justify-center">
+                <img src={project.imageUrl} alt={project.name} className="w-[60px]" />
+            </div>
+            <div>
+                <h4 className="font-medium mb-2 dark:text-white">{project.name}</h4>
+                <p className="text-sm dark:text-white">{project.tagline}</p>
+            </div>
+        </Link>
+    ))
+)}
+    </section>
+  );
+}
+
+/*
+
+ <section ref={element} className="fade translate-y-[100px] opacity-5">
       <div className="max-w-7xl mx-auto px-8 xl:px-0">
         <h2 className="text-3xl font-bold dark:text-white">Projects</h2>
         <p className="my-6">
           Among countless projects, these are my proudest achievements — <span className="italic">a testament to my dedication and creativity.</span>
         </p>
 
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-          {loading ? (
-            Array.from({ length: 5 }).map((_, i) => (
-              <Skeleton key={i} containerClassName="bg-zinc-50 p-4 rounded-lg dark:bg-zinc-800" height={60} borderRadius={'10px'} baseColor="gray" />
-            ))
-          ) : (
-            projects.map((project, i) => (
-              <Link key={i} to={`/projects/${project.id}`} className="border border-transparent bg-zinc-50 flex items-center gap-3 hover:border-gray-200 cursor-pointer p-4 rounded-lg dark:bg-zinc-800 dark:hover:border-zinc-600">
-                <div className="bg-gray-100 p-2 rounded-md w-16 h-16 md:w-[60px] md:h-[60px] flex items-center justify-center">
-                  <img src={project.imageUrl} alt={project.name} className="w-full h-full object-cover" />
-                </div>
-                <div>
-                  <h4 className="font-medium mb-2 dark:text-white">{project.name}</h4>
-                  <p className="text-sm dark:text-white">{project.tagline}</p>
-                </div>
-              </Link>
-            ))
-          )}
-        </div>
+        <section className={`projects ${project?.length <= 0 ? 'flex' : 'grid'} items-center justify-center`}>
+          {project?.length > 0 ? project : <Skeleton containerClassName="flex-1" count={5} height={60} borderRadius={'10px'} baseColor="gray" />}
+        </section>
       </div>
     </section>
-  );
-}
+*/
