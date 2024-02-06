9ef8f74e1c33cce54c083391aaf767f9e689e795
josh sofela <joshuasofela@gmail.com>
2024-02-06 21:50:11 +0100

    new commit

--- src/Components/Pages/projects.jsx
+++ src/Components/Pages/projects.jsx
@@ -17,7 +17,6 @@ export default function Projects() {
   const element = useRef(null);
   useFadeUpEffect(element);
   
-console.log(projects);
   useEffect(() => {
     document.title = 'Projects | Sofela Joshua';
 
@@ -33,15 +32,11 @@ console.log(projects);
       console.log(data.images);
       return (
         <Link key={id} to={data.id} className="border border-transparent bg-zinc-50 flex items-center gap-3 hover:border-gray-200 cursor-pointer p-4 rounded-lg dark:bg-zinc-800 dark:hover:border-zinc-600">
-          <div className="bg-gray-100 p-2 rounded-md min-w-[60px] min-h-[60px] flex items-center justify-center">
-            {
-              pic.map((pic) =>(
-                <img src={pic.image} alt="rtyn" className="w-[100px]" />
-              ))
-            }
-            
+          <div className="bg-gray-100 p-2 rounded-md flex items-center justify-center">
+            {data.images.map((imageUrl, index) => (
+              <img key={index} src={`${VITE_API_SERVER}${imageUrl}`} alt="Project Image" className="w-16 h-16 object-cover" />
+            ))}
           </div>
-
           <div>
             <h4 className="font-medium mb-2 dark:text-white">{data.title}</h4>
             <p className="text-sm dark:text-white">{data.tagline}</p>
@@ -65,19 +60,3 @@ console.log(projects);
     </section>
   );
 }
-
-/*
-
- <section ref={element} className="fade translate-y-[100px] opacity-5">
-      <div className="max-w-7xl mx-auto px-8 xl:px-0">
-        <h2 className="text-3xl font-bold dark:text-white">Projects</h2>
-        <p className="my-6">
-          Among countless projects, these are my proudest achievements — <span className="italic">a testament to my dedication and creativity.</span>
-        </p>
-
-        <section className={`projects ${project?.length <= 0 ? 'flex' : 'grid'} items-center justify-center`}>
-          {project?.length > 0 ? project : <Skeleton containerClassName="flex-1" count={5} height={60} borderRadius={'10px'} baseColor="gray" />}
-        </section>
-      </div>
-    </section>
-*/
