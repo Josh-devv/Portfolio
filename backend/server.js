a9f3381f19f53825930afa06d7c7d1b69101ebd9
josh sofela <joshuasofela@gmail.com>
2024-02-06 23:31:58 +0100

    new commit

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ app.get("/project", (req, res) => {
   });
   
 
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 8000;
  
   app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}`);
